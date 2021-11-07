0d3b8c29aa47a505dbdc05e767b80ac4562d5041
Sirikran krannate <poppurik@gmail.com>
2021-11-07 11:16:35 +0700

    Merge

---
+++ Backend/functions/verify-webhook.js
@@ -0,0 +1,19 @@
+const verifyWebhook = (req, res) => {
+  let VERIFY_TOKEN = "pusher-bot";
+
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  console.log("p-mode", mode);
+  console.log("p-token", token);
+  console.log("p-challenge", challenge);
+  console.log("p-VERIFY_TOKEN", VERIFY_TOKEN);
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+module.exports = verifyWebhook;
