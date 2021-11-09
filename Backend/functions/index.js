508b7e32f99e2be8534359ac56656230adfa054f
Sirikran krannate <poppurik@gmail.com>
2021-11-09 20:55:28 +0700

    deploy frontend complete

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,5 +1,22 @@
 const functions = require("firebase-functions");
+const express = require('express');
+    const bodyParser = require('body-parser');
+    const verifyWebhook = require('./verify-webhook');
+    //require('dotenv').config({ path: './variables.env' });
+    const messageWebhook = require('./message-webhook');
+    const app = express();
 
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    
+    app.get('/', verifyWebhook);
+    app.get('/help', (req,res)=>{
+		res.send("OK");
+	});
+    app.post('/', messageWebhook);
+
+    app.listen(5000, () => console.log('Express server is listening on port 5000'));
+exports.app = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
